8e039926521ac5d7e8e3350d3b54461f45019a4d
shallo-nairat <shallonairat@gmail.com>
2024-06-06 19:43:06 +0300

    added rows

--- index.js
+++ index.js
@@ -14,3 +14,20 @@ function generateTable() {
     }
     saveTable(); // Call saveTable() after generating the table
 }
+
+function addRow() {
+    const table = document.getElementById('dynamicTable');
+    const columns = table.rows[0].cells.length;
+    const row = table.insertRow();
+    for (let i = 0; i < columns; i++) {
+        const cell = row.insertCell();
+        cell.contentEditable = true;
+        cell.setAttribute('data-placeholder', '');
+    }
+}
+function deleteRow() {
+    const table = document.getElementById('dynamicTable');
+    if (table.rows.length > 0) {
+        table.deleteRow(table.rows.length-1);
+    }
+}
